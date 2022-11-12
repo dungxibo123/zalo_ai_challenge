ddf8565c4678886602a710e68f12a094b43d2b9c
dungxibo123 <votiendung0805@gmail.com>
2022-11-12 14:33:58 +0700

    changes env.sh

--- env.sh
+++ env.sh
@@ -4,6 +4,7 @@ pip install soundfile transformers scikit-learn click seaborn pandas pyyaml
 pip3 install https://github.com/kpu/kenlm/archive/master.zip
 pip3 install -U datasets pyctcdecode huggingface-hub
 pip install -U transformers
+pip install google google-colab
 unzip -qq ./drive/MyDrive/lyrics.zip -d ./data/train/
 unzip -qq ./drive/MyDrive/ZaloAIChallenge/vocals_data.zip -d vocals_data/
 mkdir data/
